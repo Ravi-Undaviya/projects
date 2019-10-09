313197cd4fb5216593cb0d271606a07fd6eff529
Ravi-Undaviya <56335684+Ravi-Undaviya@users.noreply.github.com>
2019-10-09 15:07:58 +0530

    Update and rename test43.py to open_url.py

---
+++ open_url.py
@@ -0,0 +1,6 @@
+import webbrowser
+import urllib.request
+# get searching keyword from the user
+google = input('Google search: ')
+# open link in new tab of your default browser
+webbrowser.open_new_tab('https://google.com/search?btnG=1&q=%s' % google)
